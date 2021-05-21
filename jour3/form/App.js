1cbcd7bc42a2a86ad34807edf87020c01ece486f
karim <karim.mezouar@gmail.com>
2021-05-21 13:59:02 +0200

    jour3-form

--- jour3/form/App.js
+++ jour3/form/App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View, Button, TextInput, Text, } from 'react-native';
+
+
+class App extends React.Component {
+  
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      nameError: '',
+      email: '',
+      emailError: '',
+      password: '',
+
+    }
+
+  }
+
+  submit() {
+    const validName = /[a - zA - Z] +$/
+    const isValid = validName.test(this.state.name)
+    console.log(isValid)
+
+    if (!isValid) {
+      this.setState({ nameError: "name field must be alphabetic " })
+    } else {
+      this.setState({ nameError: "" })
+    }
+  }
+
+  emailValidator() {
+
+    if (this.state.email == "") {
+      this.setState({ emailError: "email field can not be empty " })
+    } else {
+      this.setState({ nameError: "" })
+    }
+  }
+
+  render() {
+
+    const reg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    const validate = reg.test(this.state.email)
+
+
+    return (
+      <View style={{ margin: 20, marginTop: 100 }}>
+
+        <TextInput
+          placeholder="enter Name"
+          onChangeText={(text) => { this.setState({ name: text }) }}
+          style={{ borderWidth: 2, borderColor: 'skyblue', margin: 20 }}
+        />
+        <Text style={{ color: 'red', marginLeft: 20 }}>{this.state.nameError}</Text>
+
+        <TextInput
+          placeholder="enter email"
+          onBlur={()=>this.emailValidator()}
+          onChangeText={(text) => { this.setState({ email: text }) }}
+          style={{ borderWidth: 2, borderColor: 'skyblue', margin: 20 }}
+        />
+        <Text style={{ color: 'red', marginLeft: 20 }}>{this.state.emailError}</Text>
+
+        <TextInput
+          placeholder="enter password"
+          secureTextEntry={true}
+          maxLength = {this.state.password.length>6}
+          onChangeText={(text) => { this.setState({ password: text }) }}
+          style={{ borderWidth: 2, borderColor: 'skyblue', margin: 20 }}
+        />
+        <Button title="submit" onPress={() => { this.submit() }} />
+        {/* //   this.submit(); validate == true && this.state.password.length > 6
+           
+        //     ? alert("Connected")
+        //     : alert("you have entered an email or password not valid")
+        // }} /> */}
+
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+
+
+
+
+
